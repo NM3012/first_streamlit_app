cbd85df6b3c260f2c2dcc17f2705563793742dc6
NM3012 <98739985+NM3012@users.noreply.github.com>
2023-06-18 12:23:29 +0530

    Update streamlit_app.py
    Only showing selected fruits in the calorie list

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 # a pick-up list to allow users to pick their fruits
-streamlit.multiselect("Pick some fruits for smoothie:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits for smoothie:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruits_list.loc[fruits_selected]
 
 #Display the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
