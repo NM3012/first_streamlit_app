fbd216df5c7be537acef9a079b51a023f1e7f45c
NM3012 <98739985+NM3012@users.noreply.github.com>
2023-06-18 12:33:42 +0530

    Update streamlit_app.py
    correcting for error

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ streamlit.dataframe(my_fruit_list)
 
 # a pick-up list to allow users to pick their fruits
 fruits_selected = streamlit.multiselect("Pick some fruits for smoothie:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruits_list.loc[fruits_selected]
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #Display the table on the page
 streamlit.dataframe(fruits_to_show)
